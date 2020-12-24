158141cc5df45a2577ad17cd6c01518aa57971d5
Marcel Wiessler <marcel@gaisterhand.de>
2020-12-24 12:32:41 +0100

    init starting point, based off of jbruening/UnityFody, added debug logs, gets out of memory when trying to process assembly csharp

---
+++ projects/FodyTest/Assets/ExampleProjectWeaver/ModuleWeaver.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Fody;
+using UnityEngine;
+
+namespace ExampleProjectWeaver
+{
+    public class ModuleWeaver : BaseModuleWeaver
+    {
+
+        public override void Execute()
+        {
+            Debug.Log("Executing module weaver");//" " + ModuleDefinition.Assembly.FullName);
+        }
+
+        public override IEnumerable<string> GetAssembliesForScanning()
+        {
+            yield return "netstandard";
+            yield return "mscorlib";
+        }
+    }
+}
