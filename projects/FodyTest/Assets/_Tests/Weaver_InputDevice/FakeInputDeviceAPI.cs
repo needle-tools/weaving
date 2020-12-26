5458e151a6aad89700af27e4c5a84993b3026af4
Marcel Wiessler <marcel@gaisterhand.de>
2020-12-26 15:28:40 +0100

    moved weavers and injected target into asmdefs

--- projects/FodyTest/Assets/_Tests/Weaver_InputDevice/FakeInputDeviceAPI.cs
+++ projects/FodyTest/Assets/_Tests/Weaver_InputDevice/FakeInputDeviceAPI.cs
@@ -1,20 +1,30 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Scripting;
 using UnityEngine.XR;
+
+[assembly: Preserve]
 
 namespace _Tests.Weaver_InputDevice
 {
+	// https://docs.unity3d.com/Manual/ManagedCodeStripping.html
 	public static class FakeInputDeviceAPI
 	{
 		private static void FakeDeviceList(List<InputDevice> list)
 		{
+#if UNITY_WEBGL
+			list.Clear();
+			if(!Application.isPlaying) return;
+			if (Time.time < 2) return;
+			Debug.Log("Add devices");
 			var dev = new InputDevice();
 			list.Add(dev);
 			for (var i = 0; i < 10; i++)
 			{
-				if(Random.value > .8f)
+				if (Random.value > .8f)
 					list.Add(dev);
 			}
+#endif
 		}
 	}
 }
