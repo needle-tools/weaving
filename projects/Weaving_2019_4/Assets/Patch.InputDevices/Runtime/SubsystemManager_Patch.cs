11a23ba5849a296035af11f6a9b3a68626b805f8
Marcel Wiessler <marcel@gaisterhand.de>
2020-12-31 12:15:02 +0100

    back to actual patch state which we have to solve :)

--- projects/Weaving_2019_4/Assets/Patch.InputDevices/Runtime/SubsystemManager_Patch.cs
+++ projects/Weaving_2019_4/Assets/Patch.InputDevices/Runtime/SubsystemManager_Patch.cs
@@ -6,18 +6,13 @@ namespace needle.Weavers.InputDevicesPatch
 {
 	[NeedlePatch(typeof(SubsystemManager))]
 	public class SubsystemManager_Patch
-	{
-		private static void InternalGetDevices(List<ISubsystem> instances)
-		{
-			instances.Clear();
-			if(XRInputSubsystem_Patch.Instance is ISubsystem instance)
-				instances.Add(instance);
-			else Debug.LogError("Failed adding mock subsystem");
-		}
-		
+	{	
 		public static void GetInstances<T>(List<T> instances) where T : ISubsystem
 		{
-			InternalGetDevices(instances as List<ISubsystem>);
+			instances.Clear();
+			if(XRInputSubsystem_Patch.Instance is T instance)
+				instances.Add(instance);
+			else Debug.LogError("Failed adding mock subsystem");
 		}
 
 	}
