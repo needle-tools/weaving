c459e88aeaea47325ef040c77aa6d09934544ee4
Marcel Wiessler <marcel@gaisterhand.de>
2021-01-03 15:02:41 +0100

    can enable / disable patches in settings

--- package/Editor/Settings/WeaverSettings.cs
+++ package/Editor/Settings/WeaverSettings.cs
@@ -1,3 +1,8 @@
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEditor;
+using UnityEngine;
+
 namespace needle.Weaver
 {
 	[FilePath("ProjectSettings/NeedleWeaverSettings.asset", FilePathAttribute.Location.ProjectFolder)]
@@ -5,11 +10,28 @@ namespace needle.Weaver
 	{
 		public void Save() => Save(true);
 		public bool PatchOnBuild = false;
+
+		public bool IsDisabled(string patchFullName) => DisabledPatches.Contains(patchFullName);
+ 
+		public List<string> DisabledPatches = new List<string>();
+		
+		public void SetPatchSettingState(string fullname, bool enabled)
+		{
+			if (enabled)
+			{
+				var cnt = DisabledPatches.RemoveAll(p => p == fullname);
+			}
+			else if (!DisabledPatches.Contains(fullname))
+			{
+				DisabledPatches.Add(fullname);
+			}
+		}
 	}
 
 	[System.Serializable]
-	public class PatchConfig
+	public class SerializedPatchSetting
 	{
-		
+		public string PatchFullName;
+		public bool Enabled;
 	}
 }
--- package/Editor/Settings/WeaverSettingsProvider.cs
+++ package/Editor/Settings/WeaverSettingsProvider.cs
@@ -1,87 +1,178 @@
 using System.Collections.Generic;
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
 
 namespace needle.Weaver
 {
-    public class WeaverSettingsProvider : SettingsProvider
-    {
-        [SettingsProvider]
-        public static SettingsProvider Create()
-        {
-            try
-            {
-                WeaverSettings.instance.Save();
-                return new WeaverSettingsProvider("Project/Needle/Weaver", SettingsScope.Project);
-            }
-            catch (System.Exception e)
-            {
-                Debug.Log(e);
-            }
+	public class WeaverSettingsProvider : SettingsProvider
+	{
+		[SettingsProvider]
+		public static SettingsProvider Create()
+		{
+			try
+			{
+				WeaverSettings.instance.Save();
+				return new WeaverSettingsProvider("Project/Needle/Weaver", SettingsScope.Project);
+			}
+			catch (System.Exception e)
+			{
+				Debug.Log(e);
+			}
 
-            return null;
-        }
+			return null;
+		}
 
-        private WeaverSettingsProvider(string path, SettingsScope scopes, IEnumerable<string> keywords = null) : base(path, scopes, keywords)
-        {
-        }
-        
+		private WeaverSettingsProvider(string path, SettingsScope scopes, IEnumerable<string> keywords = null) : base(path, scopes, keywords)
+		{
+		}
 
-        private Vector2 scroll;
 
-        public override void OnGUI(string searchContext)
-        {
-            InitStyles();
+		private static WeaverSettings Settings => WeaverSettings.instance;
+		private Vector2 scroll;
+		private bool patchesFoldout = false;
 
-            EditorGUILayout.BeginVertical();
-            
-            scroll = EditorGUILayout.BeginScrollView(scroll);
-            EditorGUILayout.BeginVertical();
-            
-            WeaverSettings.instance.PatchOnBuild = EditorGUILayout.Toggle("Allow Patch on Build", WeaverSettings.instance.PatchOnBuild);
+		public override void OnGUI(string searchContext)
+		{
+			EditorGUI.indentLevel++;
+			InitStyles();
 
-            var pm = PatchMethodDatabase.AllPatchedMethods();
-            foreach (var patch in pm)
-            {
-                var enabled = EditorGUILayout.ToggleLeft(patch.FullName, patch.FoundTarget);
-            }
-            
-            EditorGUILayout.EndVertical();
-            EditorGUILayout.EndScrollView();
-            
-            GUILayout.FlexibleSpace();
-            
-            EditorGUILayout.BeginHorizontal();
-            GUILayout.FlexibleSpace();
-            EditorGUILayout.EndHorizontal();
-            
-            EditorGUILayout.EndVertical();
-        }
+			EditorGUILayout.BeginVertical();
 
-        private GUIStyle patchTitleInactive, patchTitleActive, descriptionStyle;
+			scroll = EditorGUILayout.BeginScrollView(scroll);
+			EditorGUILayout.BeginVertical();
 
-        private void InitStyles()
-        {
-            if (descriptionStyle == null)
-                descriptionStyle = new GUIStyle(EditorStyles.label)
-                {
-                    wordWrap = true,
-                    normal = {textColor = new Color(.5f, .5f, .5f)}
-                };
+			Settings.PatchOnBuild = EditorGUILayout.Toggle("Allow Patch on Build", Settings.PatchOnBuild);
 
-            if (patchTitleActive == null)
-            {
-                patchTitleActive = new GUIStyle(EditorStyles.label)
-                {
-                    fontStyle = FontStyle.Bold,
-                };
-            }
+			DrawPatchesGUI();
 
-            if (patchTitleInactive == null)
-                patchTitleInactive = new GUIStyle(patchTitleActive)
-                {
-                    normal = {textColor = new Color(.5f, .5f, .5f),},
-                };
-        }
-    }
+			EditorGUILayout.EndVertical();
+			GUILayout.Space(20);
+			EditorGUILayout.EndScrollView();
+
+			GUILayout.FlexibleSpace();
+
+			EditorGUILayout.BeginHorizontal();
+			GUILayout.FlexibleSpace();
+			EditorGUILayout.EndHorizontal();
+
+			EditorGUILayout.EndVertical();
+			EditorGUI.indentLevel--;
+		}
+
+		private GUIStyle patchTitleInactive, patchTitleActive, descriptionStyle;
+
+		private void InitStyles()
+		{
+			if (descriptionStyle == null)
+				descriptionStyle = new GUIStyle(EditorStyles.label)
+				{
+					wordWrap = true,
+					normal = {textColor = new Color(.5f, .5f, .5f)}
+				};
+
+			if (patchTitleActive == null)
+			{
+				patchTitleActive = new GUIStyle(EditorStyles.label)
+				{
+					fontStyle = FontStyle.Bold,
+				};
+			}
+
+			if (patchTitleInactive == null)
+				patchTitleInactive = new GUIStyle(patchTitleActive)
+				{
+					normal = {textColor = new Color(.5f, .5f, .5f),},
+				};
+		}
+
+		private class PatchGUIState
+		{
+			public bool Foldout;
+			public List<PatchMethodDatabase.Patch> Patches = new List<PatchMethodDatabase.Patch>();
+		}
+
+		private static readonly Dictionary<string, PatchGUIState> byName = new Dictionary<string, PatchGUIState>();
+
+		private void DrawPatchesGUI()
+		{
+			var foldoutText = "Patched Methods";
+			if (byName.Count <= 0) foldoutText += " <Will load on open>";
+			
+			EditorGUILayout.BeginHorizontal();
+			patchesFoldout = EditorGUILayout.Foldout(patchesFoldout, foldoutText, true);
+			GUILayout.FlexibleSpace();
+			EditorGUI.BeginDisabledGroup(Settings.DisabledPatches.Count <= 0);
+			if (GUILayout.Button("Reset"))
+			{
+				Settings.DisabledPatches.Clear();
+				Settings.Save();
+			}
+			EditorGUI.EndDisabledGroup();
+			EditorGUILayout.EndHorizontal();
+			
+			if (patchesFoldout)
+			{
+				EditorGUI.indentLevel++;
+				if (byName.Count <= 0)
+				{
+					var pm = PatchMethodDatabase.AllPatchedMethods();
+					foreach (var patch in pm)
+					{
+						if (!byName.ContainsKey(patch.TargetBaseName)) byName.Add(patch.TargetBaseName, new PatchGUIState() {Foldout = true});
+						byName[patch.TargetBaseName].Patches.Add(patch);
+					}
+
+					foreach (var kvp in byName)
+					{
+						kvp.Value.Patches = kvp.Value.Patches.OrderByDescending(p => p.FoundTarget).ThenBy(p => p.PatchName).ToList();
+					}
+				}
+
+				foreach (var kvp in byName)
+				{
+					var name = kvp.Key;
+					var state = kvp.Value;
+					var patches = state.Patches;
+					EditorGUILayout.BeginHorizontal();
+					state.Foldout = EditorGUILayout.Foldout(state.Foldout, name + " [" + patches.Count + "]", true);
+					GUILayout.FlexibleSpace();
+					if (GUILayout.Button("Invert"))
+					{
+						foreach (var patch in patches)
+						{
+							Settings.SetPatchSettingState(patch.TargetFullName, Settings.IsDisabled(patch.TargetFullName));
+						}
+
+						Settings.Save();
+					}
+
+					EditorGUILayout.EndHorizontal();
+					if (state.Foldout)
+					{
+						EditorGUI.indentLevel++;
+						foreach (var patch in patches)
+						{
+							EditorGUI.BeginDisabledGroup(!patch.FoundTarget);
+							EditorGUI.BeginChangeCheck();
+							var ps = EditorGUILayout.ToggleLeft(
+								new GUIContent(patch.PatchName, patch.TargetFullName + "\n" + patch.Target + "\nin " + patch.TargetFullName),
+								!Settings.IsDisabled(patch.TargetFullName));
+							if (EditorGUI.EndChangeCheck())
+							{
+								Settings.SetPatchSettingState(patch.TargetFullName, ps);
+								Settings.Save();
+							}
+
+							EditorGUI.EndDisabledGroup();
+						}
+
+						EditorGUI.indentLevel--;
+					}
+				}
+
+				EditorGUI.indentLevel--;
+			}
+		}
+	}
 }
